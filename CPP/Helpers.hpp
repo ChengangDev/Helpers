078376ccb291561fb5b177ba3e34f597104acf92
ChengangDev <chengang2022@gmail.com>
2016-03-28 11:03:45 +0800

    Update Helpers.hpp
    add stringbuilder

--- CPP/Helpers.hpp
+++ CPP/Helpers.hpp
@@ -55,3 +55,15 @@ class Helpers
             return elems;
         }
 };
+
+struct stringbuilder
+{
+   std::stringstream ss;
+   template<typename T>
+   stringbuilder & operator << (const T &data)
+   {
+        ss << data;
+        return *this;
+   }
+   operator std::string() { return ss.str(); }
+};
